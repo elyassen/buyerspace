631749b9d9850ce1ab4d8cd5410ec0d7bbddb996
yassen <yaseenseen9@gmail.com>
2024-04-19 11:14:18 +0530

    server sttart

--- src/Home.js
+++ src/Home.js
@@ -1,20 +1,38 @@
-import React, { Suspense, useEffect } from "react";
+import React, { useEffect, useState } from "react";
 import Categories from "./components/Categories";
 import Header from "./components/Header";
 import { BASE_URL } from "./utils/utils";
-// const Products = React.lazy(() => import("./components/Products"));
 import Products from "./components/Products";
+import { CloseCircleFilled } from "@ant-design/icons";
 function Home() {
+  const [Server, setServer] = useState(false);
   useEffect(() => {
     async function startServer() {
       const req = await fetch(`${BASE_URL}/`);
+      const res = await req.json();
+      console.log(res);
+      if (res.status === "true") {
+        setServer(true);
+      }
     }
     startServer();
   }, []);
   return (
     <>
-      <Categories />
-      <Products />
+      {Server ? (
+        <>
+          <Categories />
+          <Products />
+        </>
+      ) : (
+        <div className="server-start-div">
+          <h2>
+            Server inactive. Activating shortly. Hosted on Render free-tier
+          </h2>
+          <div className="loading-server"></div>
+          <p>please wait..</p>
+        </div>
+      )}
     </>
   );
 }
